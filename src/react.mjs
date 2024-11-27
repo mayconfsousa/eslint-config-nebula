2e6551c3db7a3f7d26dfb77c441a8d43d3c223c8
Maycon Sousa <mayconfsousa@gmail.com>
2024-11-27 11:59:56 -0300

    feat: add eslint-plugin-react-refresh
    Release-As: 1.0.7

--- src/react.mjs
+++ src/react.mjs
@@ -1,5 +1,6 @@
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tailwind from 'eslint-plugin-tailwindcss'
 import globals from 'globals'
 
@@ -18,11 +19,11 @@ export default [
   {
     plugins: {
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
-    rules: reactHooks.configs.recommended.rules,
-  },
-  {
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react/jsx-boolean-value': 'warn',
       'react/self-closing-comp': 'warn',
     },
